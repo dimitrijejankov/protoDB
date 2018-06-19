e371f76f7ef96923fec3e4bd6fc5904b0a9f01e7
Dimitrije Jankov <dimitrijejankov@gmaik.com>
2018-06-19 11:58:37 -0500

    Moved some stuff

--- script/install.sh
+++ script/install.sh
@@ -4,6 +4,3 @@
 
 sudo apt-get update
 sudo apt-get install -y cmake mpich libgsl-dev libboost-all-dev libatomic-ops-dev libatomic1
-sudo add-apt-repository ppa:jonathonf/gcc-7.1
-sudo apt-get update
-sudo apt-get install -y gcc-7 g++-7
